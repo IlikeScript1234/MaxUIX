6ee23742b831a9f245f460b6c1fb7e5dfceb1e04
MaxlaserTech <104206888+IlikeScript1234@users.noreply.github.com>
2024-01-02 01:47:25 +0700

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -503,6 +503,42 @@ function MainLoaded()
     
     UIAspectRatioConstraint_20.Parent = ScreenGui
     UIAspectRatioConstraint_20.AspectRatio = 2.047
+    local UIS = game:GetService("UserInputService")
+	local TweenService = game:GetService("TweenService")
+	
+	local button = Frame
+	local dragToggle = nil
+	local dragSpeed = 0.35
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(button, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	button.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = button.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
 end
 
 task.spawn(function()
